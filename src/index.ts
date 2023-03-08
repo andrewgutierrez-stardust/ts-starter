77e9cc8fbf8f44a3dbf533551f2b527a1229539f
andrew <andrew.gutierrez@stardust.gg>
2023-03-08 12:26:56 -0800

    create starter ts project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World");
+async function main() {
+  console.log("Hello World");
+}
+
+main()
+  .then(() => {
+    console.log("Done");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
